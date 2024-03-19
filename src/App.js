c5e0e1e8fafcecbb2cfa5653e6f39df9039e051f
tiwaskarkranti20 <tiwaskarkranti20@gmail.com>
2024-03-19 09:53:58 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React, { useState } from 'react';
+import { Layout, Menu, Select } from 'antd';
+import Transactions from './components/Transactions';
+import Stats from './components/Stats';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+
+
+const { Header, Content, Footer } = Layout;
+
+
+const navItems = [
+  {
+    key: 1,
+    label: ( <NavLink to="/">Transactions</NavLink> )
+  },
+  {
+    key: 2,
+    label: ( <NavLink to="/stats">Stats</NavLink> )
+  }
+];
+const options = [
+  "All Months",
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+
+const App = () => {
+  let [month, setMonth] = useState(3);
+
+  const handleMonthChange = (value) => {
+    setMonth(parseInt(value));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <BrowserRouter>
+      <Layout>
+        <Header
+          style={{
+            display: "flex",
+            alignItems: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <h1 style={{ color: "white" }}>Dashboard</h1>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={["1"]}
+            items={navItems}
+            style={{
+              flex: 1,
+              padding: "0 60px"
+            }}
+          />
+          <Select
+            size="large"
+            defaultValue={options[month]}
+            onChange={handleMonthChange}
+            style={{
+              width: 200
+            }}
+            options={options.map((text, i) => {
+              return {
+                value: i,
+                label: text
+              };
+            })}
+          />
+        </Header>
+        <Content
+          style={{
+            padding: "0px 48px",
+            backgroundColor: "white",
+            minHeight: 600
+          }}
+        >
+          <Routes>
+            <Route path="/" element={
+              <Transactions month={month} monthText={options[month]} />
+            } />
+            <Route path="/stats" element={
+              <Stats month={month} monthText={options[month]} />
+            } />
+          </Routes>
+
+        </Content>
+        <Footer
+          style={{
+            textAlign: "center"
+          }}
+        >
+        
+        </Footer>
+      </Layout>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
